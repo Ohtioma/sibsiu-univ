1cc9859b1b676d8f605574b29ce3cbe5b6ed78d2
Ohtioma <124048542+Ohtioma@users.noreply.github.com>
2025-10-20 01:32:47 +0700

    Add files via upload
    Commit message: "Initial commit: базовая структура проекта"

---
+++ Git_proect/js/main.js
@@ -0,0 +1,7 @@
+// Основные функции сайта
+console.log('Сайт "Качество в СибГИУ" загружен');
+
+// Навигация и базовые взаимодействия
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM готов');
+});
---
+++ Git_proect/js/poll-builder.js
@@ -0,0 +1,10 @@
+// Конструктор опросов - будет заполнен позже
+class PollBuilder {
+    constructor() {
+        this.questions = [];
+    }
+    
+    addQuestion(question) {
+        this.questions.push(question);
+    }
+}
