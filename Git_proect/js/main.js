206fb0865c5122a1060d81061516c8c1b8d11f87
Ohtioma <124048542+Ohtioma@users.noreply.github.com>
2026-03-22 20:26:33 +0700

    Delete Git_proect directory

--- Git_proect/js/main.js
+++
@@ -1,7 +0,0 @@
-// Основные функции сайта
-console.log('Сайт "Качество в СибГИУ" загружен');
-
-// Навигация и базовые взаимодействия
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('DOM готов');
-});
